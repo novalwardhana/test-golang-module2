a1a8e57892f13ff51687f89084dad4a7b92b3840
novalwardhana <novalita.k.wardhana@gmail.com>
2022-04-30 10:00:37 +0700

    test 2

--- cube/mod_cube.go
+++ cube/mod_cube.go
@@ -7,10 +7,12 @@ type Cube struct {
 	Height float64
 }
 
+/* Surface area */
 func (c Cube) SurfaceArea() float64 {
 	return 2*c.Length*c.Wide + 2*c.Wide*c.Height + 2*c.Length*c.Height
 }
 
+/* Volume */
 func (c Cube) Volume() float64 {
 	return c.Length * c.Wide * c.Height
 }
